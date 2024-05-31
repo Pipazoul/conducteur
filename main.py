df188a9cc86c4d400e7069a47b4577ec81e00e35
Yassin Siouda <yassin@siouda.com>
2024-05-31 21:43:49 +0200

    refact: create functions

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
 import json
-import time
+import os
+import random
 import requests
+import time
 from datetime import datetime, timedelta
+from threading import Thread
 from fastapi import FastAPI, HTTPException
-from fastapi import Request
+from pydantic import BaseModel
 from docker import DockerClient
 import docker.types
-from threading import Thread
-from pydantic import BaseModel
-import os
 import dotenv
-import random
+from fastapi.requests import Request
+
 
 dotenv.load_dotenv()
-
 app = FastAPI()
+
+# Load environment variables
 ssh_user = os.getenv("DOCKER_SSH_USER")
 ssh_key_path = os.getenv("DOCKER_SSH_KEY_PATH")
 ssh_host = os.getenv("DOCKER_SSH_HOST")
-client = None
+client = DockerClient(base_url=f"ssh://{ssh_user}@{ssh_host}" if ssh_key_path else "unix://var/run/docker.sock")
 
-if ssh_host:
-    if ssh_key_path:
-        print("Using SSH key for remote Docker access.")
-        base_url = f"ssh://{ssh_user}@{ssh_host}"
-        client = DockerClient(base_url=base_url, use_ssh_client=True)
-    else:
-        print("SSH credentials or key must be provided for remote Docker access.")
-else:
-    client = DockerClient(base_url="unix://var/run/docker.sock")
-
+# Initialize jobs dictionary
 jobs = {}
 
 
@@ -41,28 +34,49 @@ def load_jobs():
         return {}
 
 
+jobs = load_jobs()
+
+
 def save_jobs():
     with open("jobs.json", "w") as file:
         json.dump(jobs, file, indent=4)
 
 
+def get_container_by_image(image):
+    for container in client.containers.list(all=True):
+        if container.attrs["Config"]["Image"] == image:
+            return container
+    return None
+
+
+def start_or_restart_container(container, image):
+    if container.status == "exited":
+        stop_containers()
+        container.start()
+        container.reload()
+    port_mappings = container.attrs["NetworkSettings"]["Ports"]
+    return port_mappings["5000/tcp"][0]["HostPort"] if "5000/tcp" in port_mappings else None
+
+
+def stop_containers():
+    for container in client.containers.list():
+        port_info = container.attrs["NetworkSettings"]["Ports"]["5000/tcp"]
+        if port_info:
+            port = port_info[0]["HostPort"]
+            if 6000 <= int(port) <= 6600:
+                container.stop()
+
+
 def health_check_routine(job_id, container_id, port):
-    print("Starting health check routine...")
     container = client.containers.get(container_id)
     start_time = datetime.now()
     while datetime.now() - start_time < timedelta(minutes=4):
         try:
             response = requests.get(f"http://{ssh_host}:{port}/health-check")
-            if response.status_code == 200:
-                # check if status is READY
-                status = response.json()["status"]
-
-                if status == "READY":
-                    jobs[job_id]["status"] = "predicting"
-                    save_jobs()
-                    print("Job is ready for prediction.")
-                    return
-
+            if response.status_code == 200 and response.json().get("status") == "READY":
+                jobs[job_id]["status"] = "predicting"
+                save_jobs()
+                return
         except requests.exceptions.RequestException:
             pass
         time.sleep(0.2)
@@ -72,90 +86,24 @@ def health_check_routine(job_id, container_id, port):
     save_jobs()
 
 
-def stop_containers():
-    # Stop containers that are not using the required image
-    for container in client.containers.list():
-        port_info = container.attrs["NetworkSettings"]["Ports"]["5000/tcp"]
-        if port_info:
-            port = port_info[0]["HostPort"]
-            if 6000 <= int(port) <= 6600:
-                container.stop()
-                print(f"Stopped container on port {port}")
-
-
 @app.post("/jobs/")
 async def add_job(image: str):
-    print("Adding job...")
-    existing_container = None
-    port = None
-
-    # List all containers, including those that are stopped
-    for container in client.containers.list(all=True):
-        container_image = container.attrs["Config"]["Image"]
-        if container_image == image:
-            print(f"Found matching container with image: {container_image}")
-            existing_container = container
-            if container.status == "running":
-                port_mappings = container.attrs["NetworkSettings"]["Ports"]
-                port = (
-                    port_mappings["5000/tcp"][0]["HostPort"]
-                    if "5000/tcp" in port_mappings
-                    else None
-                )
-                break
-            elif container.status == "exited":
-                stop_containers()
-                container.start()
-                container.reload()
-                port_mappings = container.attrs["NetworkSettings"]["Ports"]
-                port = (
-                    port_mappings["5000/tcp"][0]["HostPort"]
-                    if "5000/tcp" in port_mappings
-                    else None
-                )
-                print(f"Restarted stopped container with image: {image} on port {port}")
-                break
-
-    if existing_container:
-        job_id = str(existing_container.id)
-        print(f"Using existing/restarted container: {job_id}")
-        jobs[job_id] = {
-            "image": image,
-            "status": "running",
-            "started_at": str(datetime.now()),
-            "port": port,
-        }
-        save_jobs()
+    container = get_container_by_image(image)
+    if container:
+        port = start_or_restart_container(container, image)
+        job_id = str(container.id)
     else:
-        # If no suitable container is found, create a new one
         if any(job["status"] == "running" for job in jobs.values()):
-            raise HTTPException(
-                status_code=400, detail="Another job is currently running."
-            )
-
+            raise HTTPException(status_code=400, detail="Another job is currently running.")
         stop_containers()
         port = random.randint(6000, 6600)
-        container = client.containers.run(
-            image,
-            detach=True,
-            ports={"5000/tcp": port},
-            device_requests=[
-                docker.types.DeviceRequest(count=-1, capabilities=[["gpu"]])
-            ],
-        )
+        container = client.containers.run(image, detach=True, ports={"5000/tcp": port}, device_requests=[docker.types.DeviceRequest(count=-1, capabilities=[["gpu"]])])
         job_id = str(container.id)
-        jobs[job_id] = {
-            "image": image,
-            "status": "running",
-            "started_at": str(datetime.now()),
-            "port": port,
-        }
-        save_jobs()
-    print("port before health check", port)
-    Thread(
-        target=health_check_routine, args=(job_id, container.id, port), daemon=True
-    ).start()
 
+    jobs[job_id] = {"image": image, "status": "running", "started_at": str(datetime.now()), "port": port}
+    save_jobs()
+
+    Thread(target=health_check_routine, args=(job_id, container.id, port), daemon=True).start()
     return {"job_id": job_id}
 
 
@@ -171,64 +119,48 @@ class PredictionData(BaseModel):
 
 @app.post("/predict")
 async def predict(request: Request):
-    print("Predicting...")
     data = await request.json()
-    image = data["image"]
-    input = data["input"]
-    print(f"Predicting for image: {image}")
-    print(f"Input: {input}")
-
-    # Add a new job and start the health check
-    job_response = await add_job(image)
+    job_response = await add_job(data["image"])
     job_id = job_response["job_id"]
-    job = jobs[job_id]
+    return await handle_prediction(job_id, data["input"])
 
-    # Wait until the job status changes from 'running' or until timeout
+
+async def handle_prediction(job_id, input):
+    job = jobs[job_id]
     start_time = datetime.now()
     timeout = timedelta(minutes=3)
+
     while job["status"] == "running" and datetime.now() - start_time < timeout:
         time.sleep(0.2)
         job = jobs[job_id]
-    print("outside while loop")
-    print(job)
-    # Check if the health check passed and the job is ready for prediction
+
     if job["status"] == "predicting":
-        port = job["port"]
-        try:
-            # Attempt to get a prediction from the container
-            response = requests.post(
-                f"http://{ssh_host}:{port}/predictions", json={"input": input}
-            )
-            if response.status_code == 200:
-                # If successful, remove the job and save the jobs state
-                jobs.pop(job_id, None)
-                save_jobs()
-                return response.json()
-            else:
-                # return the error message
-                data = response.json()
-                print(data)
-                raise HTTPException(status_code=500)
-        except requests.exceptions.RequestException as e:
-            raise HTTPException(status_code=500, detail=str(e))
+        response = await make_prediction(job_id, job["port"], input)
+        return response
     else:
-        # If the job failed or timed out, raise an exception
-        jobs.pop(job_id, None)
-        save_jobs()
-        if job["status"] == "failed":
-            detail = "Job failed during execution."
+        handle_job_failure(job_id, job["status"])
+
+
+async def make_prediction(job_id, port, input):
+    try:
+        response = requests.post(f"http://{ssh_host}:{port}/predictions", json={"input": input})
+        if response.status_code == 200:
+            jobs.pop(job_id, None)
+            save_jobs()
+            return response.json()
         else:
-            detail = "Job timed out or failed."
-        raise HTTPException(status_code=408, detail=detail)
+            raise HTTPException(status_code=500)
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+def handle_job_failure(job_id, status):
+    jobs.pop(job_id, None)
+    save_jobs()
+    detail = "Job failed during execution." if status == "failed" else "Job timed out or failed."
+    raise HTTPException(status_code=408, detail=detail)
 
 
 if __name__ == "__main__":
-    jobs = load_jobs()
-    try:
-        import uvicorn
-
-        uvicorn.run(app, host="0.0.0.0", port=8000)
-    except KeyboardInterrupt:
-        print("Shutting down gracefully...")
-        client.close()
-        print("Cleanup complete, application stopped.")
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
