58ae3cdc107c75fd21f9a1443a1727b5e557221f
Yassin Siouda <yassin@siouda.com>
2024-06-01 20:07:36 +0200

    fix: add header token for benchmark

--- benchmark.py
+++ benchmark.py
@@ -5,7 +5,10 @@ import time
 
 # The URL and headers for the request
 url = 'http://localhost:8000/predict'
-headers = {'Content-Type': 'application/json'}
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer mytoken'
+    }
 data = json.dumps({
     "image": "r8.im/stability-ai/sdxl@sha256:7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc",
     "input": {
