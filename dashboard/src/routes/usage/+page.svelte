b15d9ce882d1d0feb5a6fa896fb58a4cc91fdb26
Yassin Siouda <yassin@siouda.com>
2024-06-11 17:23:56 +0200

    feat(dashoard): display image usage

--- dashboard/src/routes/usage/+page.svelte
+++ dashboard/src/routes/usage/+page.svelte
@@ -11,7 +11,7 @@
         '6m': 180,
         '1y': 365,
     }
-    let currentFilter = '1d'
+    let currentFilter = '1m'
 
 
     $: {
@@ -37,12 +37,15 @@
         })
         const data = await res.json()
         predictions = data
-        console.log("LOL Predictions", predictions)
+        await imageDuration()
     }
 
     async function imageDuration() {
         // Create a new Set
         let imageSet = new Set();
+
+        // Calculate total duration
+        let totalDuration = predictions.predictions.reduce((total, prediction) => total + prediction.duration, 0);
 
         // For each prediction
         for (let i = 0; i < predictions.predictions.length; i++) {
@@ -63,11 +66,16 @@
             }
         }
 
+        // Calculate percentage for each image and add it to the set
+        imageSet = new Set(Array.from(imageSet).map(entry => ({
+            ...entry,
+            percentage: (entry.sumDuration / totalDuration) * 100
+        })));
+
         // Replace the old set with the new one
         images = imageSet;
     }
         
-
     function formatDuration(duration: number) {
         const hours = Math.floor(duration / 3600)
         const minutes = Math.floor((duration % 3600) / 60)
@@ -103,6 +111,16 @@
     <h2 class="mt-4">Overall Usage</h2>
     <div class="border rounded-md mt-4 p-4">
         Total duration: {formatDuration(predictions?.totalDuration)}
+        <div class="divider"></div>
+        {#each images as image}
+            <div>
+                <p class="w-full overflow-x-scroll">{image?.image}</p>
+                <p>{formatDuration(image?.sumDuration)}</p>
+                <progress class="progress progress-accent w-full" value="{image?.percentage || 0}" max="100"></progress>
+            </div>
+        <div class="divider"></div>
+
+        {/each}
 
     </div>
 
