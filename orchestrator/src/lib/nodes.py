ede397b8ffb77417cc8af0d7c1e77ed827d2e1a4
Yassin Siouda <yassin@siouda.com>
2024-07-13 17:31:10 +0200

    feat(orchestrator): wip handle mutiple predictions

--- orchestrator/src/lib/nodes.py
+++ orchestrator/src/lib/nodes.py
@@ -1,7 +1,43 @@
 import lib.conf as conf
+import json
 config = conf.load()
 nodesPath = config["logs"]["nodesPath"]
 
 
-def initNodeState():
+def state() -> dict:
+    with open(nodesPath, 'r') as file:
+        return json.load(file)
     
+def initNodeState():
+    nodes = config["nodes"]
+    nodesState = []
+    for node in nodes:
+        # clear the node state file and append []
+        with open(nodesPath, 'w') as outfile:
+            json.dump([], outfile)
+        nodeState = { "name": node["name"],"host": node["host"] ,"user": node["user"], "state": "available", "weight": node["weight"], "rsa": node["rsa"]}
+        nodesState.append(nodeState)
+    with open(nodesPath, 'w') as outfile:
+        json.dump(nodesState, outfile)
+
+
+def getAvailableNode() -> dict:
+    nodes = state()
+    
+    # Find the most heavy available node
+    available_nodes = [n for n in nodes if n["state"]  == "available"]
+    if available_nodes:
+        max_weight_node = max(available_nodes, key=lambda k: k['weight'])
+        return max_weight_node
+    else:
+        # Handle the case where there are no available nodes
+        return None
+
+def updateState(name, nodeState):
+    nodes = state()
+    for node in nodes:
+        if node["name"] == name:
+            node["state"] = nodeState
+    
+    with open(nodesPath, 'w') as outfile:
+        json.dump(nodes, outfile)
