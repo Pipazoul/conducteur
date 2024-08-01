d3e9b25fc41d3e548cfd11227958a8a59c8fb3b1
Yassin Siouda <yassin@siouda.com>
2024-08-01 20:59:46 +0200

    chore(orchestrator): cluster prediction runner

--- orchestrator/src/main.py
+++ orchestrator/src/main.py
@@ -4,6 +4,7 @@ from lib.config import Config
 from lib.auth import Authenticate
 from lib.nodes import  Cluster
 
+import datetime
 
 config = Config()
 app = FastAPI()
@@ -11,36 +12,22 @@ auth = Authenticate()
 
 cluster = Cluster(config.nodes)
 
-
-
-# TEST #############################
-prediction = Predictions(
-    user="John Doe", 
-    image="docker image", 
-    started="2022-03-17 10:00:00", 
-    finished="2022-03-17 10:10:00", 
-    duration=10.0
-)
-
-prediction.add()
-
-
-
-###########################################
-
-
-
-
 @app.post("/predict")
 async def predict(
     request: Request, 
 ):
     data = await request.json()
     image = data["image"]
-    Authenticate.verify_existing_token(Authenticate.extract_token(request))
+    token = Authenticate.extract_token(request)
+    Authenticate.verify_existing_token(token)
     Authenticate.verify_image_scope(Authenticate.extract_token(request), image)
-    print(cluster.nodes)
-    return {"cluster": str(cluster.nodes)}
+    new_prediction = Predictions(
+        user= config.get_user_by_token(token),
+        image=image, 
+        started= datetime.datetime.now(),
+    )
+    cluster.run_prediction(new_prediction)
+    return new_prediction
 
 
 
