348418cbc447391521521fa7f669b29b4cf2206d
Yassin Siouda <yassin@siouda.com>
2024-08-02 19:00:11 +0200

    feat(orchestrator): multiple node handling

--- orchestrator/src/lib/docker.py
+++ orchestrator/src/lib/docker.py
@@ -1,11 +1,11 @@
 import random
 import docker.types
+from fastapi import HTTPException
 
 class Docker:
     def __init__(self, client):
         self.client = client
     def stop_containers(self):
-        
         for container in self.client.containers.list():
             ports = container.attrs["NetworkSettings"]["Ports"]
             if '5000/tcp' in ports and ports['5000/tcp'] is not None:
@@ -13,15 +13,15 @@ class Docker:
                 if port_info and 6000 <= int(port_info["HostPort"]) <= 6600:
                     container.stop()
             else:
-                print(f"No '5000/tcp' port mapping found for container {container.id}")
+                return
 
     def start_or_restart_container(self,container):
         if container.status == "exited":
             self.stop_containers()
-            self.client.start()
-            self.client.reload()
+            container.start()
+            container.reload()
         port_mappings = container.attrs["NetworkSettings"]["Ports"]
-        return port_mappings["5000/tcp"][0]["HostPort"] if "5000/tcp" in port_mappings else None
+        return container
     
     def get_container_by_image(self,image):
         for container in self.client.containers.list(all=True):
@@ -32,8 +32,13 @@ class Docker:
     def run_container(self,image):
         self.stop_containers()
         port = random.randint(6000, 6600)
-        container = self.client.containers.run(image, detach=True, ports={"5000/tcp": port}, device_requests=[docker.types.DeviceRequest(count=-1, capabilities=[["gpu"]])])
-        return container, port
+        try:
+            container = self.client.containers.run(image, detach=True, ports={"5000/tcp": port}, device_requests=[docker.types.DeviceRequest(count=-1, capabilities=[["gpu"]])])
+            return container
+        except(docker.errors.APIError) as e:
+            raise HTTPException(status_code=403, detail="Could not run the docker image due to "+str(e))
+
+
     
     def get_container(self,container_id):
         return self.client.containers.get(container_id)
--- orchestrator/src/lib/predictions.py
+++ orchestrator/src/lib/predictions.py
@@ -1,6 +1,6 @@
 from enum import Enum
 import sqlite3
-con = sqlite3.connect("conducteur.db")
+con = sqlite3.connect("conducteur.db", check_same_thread=False)
 cur = con.cursor()
 
 class PredictionStatus(Enum):
@@ -10,10 +10,11 @@ class PredictionStatus(Enum):
     failed = "failed"
         
 class Predictions:
-    def __init__(self, user:str, image:str, started:str, finished:str = None, duration:float = None):
+    def __init__(self, user:str, image:str, input:str ,started:str, finished:str = None, duration:float = None):
             self.id = None  # will be set by the database when inserting a new row.
             self.user = user
             self.image = image
+            self.input = input  # the actual data that will be predicted.
             self.status = PredictionStatus.pending.value
             self.started = started
             self.finished = finished
@@ -33,16 +34,54 @@ class Predictions:
             con.commit()
             self.id = cur.lastrowid
             
+    def update(self):
+        cur.execute("UPDATE prediction SET status = ? , finished = ? , duration = ? WHERE id = ?", (self.status, self.finished, self.duration, self.id))
+        con.commit()
 
-            
-        
-    def filter_by_user(self, user: str):
+    @staticmethod
+    def filter_by_user(user: str):
         cur.execute("SELECT * FROM prediction WHERE user = ?", (user, ))
-        return cur.fetchall()
+        response = cur.fetchall()
+        data = []
+        # return obj {'user': '...', 'image': '...', 'status': '...'} for each row in the db
+        for row in response:
+            data.append({
+                'id': int(row[0]),
+                'user': row[1],
+                'image': row[2],
+                'status': row[3],
+                'started': str(row[4]),
+                'finished': str(row[5]),
+                'duration': float(row[6]) if row[6] else None,
+            })
+
+        return data
+
     
-    def get(self):
+    def get_one(self, id: int):
+        cur.execute("SELECT * FROM prediction WHERE id = ?", (id,))
+    
+    @staticmethod
+    def get_all():
         cur.execute('''SELECT * FROM prediction ''')
-        return cur.fetchall()
+        response = cur.fetchall()
+        data = []
+        # return obj {'user': '...', 'image': '...', 'status': '...'} for each row in the db
+        for row in response:
+            data.append({
+                'id': int(row[0]),
+                'user': row[1],
+                'image': row[2],
+                'status': row[3],
+                'started': str(row[4]),
+                'finished': str(row[5]),
+                'duration': float(row[6]) if row[6] else None,
+            })
+
+        return data
+
+    
+
         
 
 
