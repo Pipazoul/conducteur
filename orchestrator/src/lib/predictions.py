c69d61b7c126f73bfdbfe330472f3a9b3c99e8d6
Yassin Siouda <yassin@siouda.com>
2024-08-11 17:06:38 +0200

    chore: dashboard update and many orchestrator fixes

--- orchestrator/src/lib/predictions.py
+++ orchestrator/src/lib/predictions.py
@@ -12,7 +12,7 @@ class PredictionStatus(Enum):
         
 class Predictions:
     _lock = Lock()
-    def __init__(self, user:str, image:str, input:str ,started:str, node: str, finished:str = None, duration:float = None, co2=None, status:PredictionStatus = PredictionStatus.pending):
+    def __init__(self, user:str, image:str, input:str ,started:str, node: str=None, finished:str = None, duration:float = None, co2=None, status:PredictionStatus = PredictionStatus.pending):
             self.id = None  # will be set by the database when inserting a new row.
             self.user = user
             self.image = image
@@ -23,22 +23,22 @@ class Predictions:
             self.finished = finished
             self.duration = duration
             self.co2 = co2
-            
-    def create(self):
-        cur.execute('''
+            cur.execute('''
                 CREATE TABLE IF NOT EXISTS prediction(
                 id INTEGER PRIMARY KEY,
                 user TEXT NOT NULL,
                 image TEXT NOT NULL,
                 status TEXT NOT NULL,
                 started DATETIME NOT NULL,
-                node TEXT NOT NULL,
+                node TEXT,
                 finished DATETIME,
                 duration FLOAT,
                 co2 FLOAT);
             ''')
-        cur.execute("INSERT INTO prediction (user,image,status,started,finished,duration) VALUES (?, ?, ?, ?, ?, ?)", 
-                (self.user, self.image, self.status, self.started, self.finished, self.duration))
+            
+    def create(self):
+        cur.execute("INSERT INTO prediction (user,image,status,started,node,finished,duration) VALUES (?, ?, ?, ?, ?, ?, ?)", 
+                (self.user, self.image, self.status, self.started, self.node, self.finished, self.duration))
         con.commit()
         self.id = cur.lastrowid
         
@@ -53,6 +53,8 @@ class Predictions:
         cur.execute("SELECT * FROM prediction WHERE user = ?", (user, ))
         response = cur.fetchall()
         data = []
+        total_co2 = 0
+        total_duration = 0
         # return obj {'user': '...', 'image': '...', 'status': '...'} for each row in the db
         for row in response:
             data.append({
@@ -61,12 +63,14 @@ class Predictions:
                 'image': row[2],
                 'status': row[3],
                 'started': str(row[4]),
-                'finished': str(row[5]),
-                'duration': float(row[6]) if row[6] else None,
-                'co2': float(row[7]) if row[7] else None,
+                'node': str(row[5]),
+                'finished': str(row[6]),
+                'duration': float(row[7]) if row[7] else None,
+                'co2': float(row[8]) if row[8] else None,
             })
-
-        return data
+            total_co2 += float(row[8]) if row[8] else 0
+            total_duration += float(row[7]) if row[7] else 0
+        return ({"total_co2": total_co2, "total_duration":total_duration, "predictions":data})
 
     
     def get_one(self, id: int):
@@ -85,8 +89,10 @@ class Predictions:
                 'image': row[2],
                 'status': row[3],
                 'started': str(row[4]),
-                'finished': str(row[5]),
-                'duration': float(row[6]) if row[6] else None,
+                'node': str(row[5]),
+                'finished': str(row[6]),
+                'duration': float(row[7]) if row[7] else None,
+                'co2': float(row[8]) if row[8] else None,
             })
 
         return data
